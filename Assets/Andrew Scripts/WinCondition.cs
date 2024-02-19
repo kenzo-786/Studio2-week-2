1caeb15383d40254ef0385e835086b346b25a39f
Andrew Roberts <a.roberts@student.saedubai.com>
2024-02-20 00:20:34 +0400

    Win Condition Added

--- Assets/Andrew Scripts/WinCondition.cs
+++ Assets/Andrew Scripts/WinCondition.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinCondition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.tag == "Win1")
+        {
+            SceneManager.LoadScene("level 2");
+        }
+        if (collision.gameObject.tag == "Win2")
+        {
+            SceneManager.LoadScene("Main menu");
+        }
     }
 }
